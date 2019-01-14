01ed024d77ac9be68fdf4b5b529e854ed14a0c12
iRED <foodrickme@gmail.com>
2019-01-13 20:04:43 -0800

    Updates

---
+++ precache-manifest.42fc4bd73aa51533dc4562bd10618bf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37819f706965bbbf4355",
+    "url": "/bls-stats/static/css/main.703646db.chunk.css"
+  },
+  {
+    "revision": "37819f706965bbbf4355",
+    "url": "/bls-stats/static/js/main.37819f70.chunk.js"
+  },
+  {
+    "revision": "a0b60377fa53e4939e60",
+    "url": "/bls-stats/static/js/1.a0b60377.chunk.js"
+  },
+  {
+    "revision": "98a11a0b3ed912aa652c",
+    "url": "/bls-stats/static/js/runtime~main.98a11a0b.js"
+  },
+  {
+    "revision": "c0f66897e8ae80630876fc34ec7d9fd9",
+    "url": "/bls-stats/index.html"
+  }
+];
